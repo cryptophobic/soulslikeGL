60f5bd1d89afe234c5847d5f0366e15f79551825
cryptophobic <dmitryuglach@gmail.com>
2022-09-04 18:09:23 +0300

    Cross-platform

--- utils/filesystem_helper.cpp
+++ utils/filesystem_helper.cpp
@@ -14,6 +14,6 @@ std::string FileSystemHelper::getPath(const std::string& path) {
 void FileSystemHelper::setApplicationPath(std::string path) {
     boost::replace_all(path,"./", "");
     const fs::path fullApplicationPath = fs::absolute(fs::path(path));
-    root = fullApplicationPath.parent_path().parent_path();
+    root = fullApplicationPath.parent_path().parent_path().string();
     std::cout << root << " set" << std::endl;
 }
